79e009421ebb042671014efa5429d2ad4d2b03e6
amidman <amidman04@gmail.com>
2024-08-16 20:49:06 +0300

    create

---
+++ lin_op.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void nabla(float* A, Vec3* V, int N, int M){
+
+}
---
+++ solver.h
@@ -0,0 +1,33 @@
+#include"types.h"
+#include"lin_op.h"
+
+class Solver{
+    public:
+
+    Vec3field* V_old;
+    floatfield* P_old;
+    floatfield* ro_old;
+
+    Vec3field* V_new;
+    floatfield* P_new;
+    floatfield* ro_new;
+
+    int N;
+    int M;
+
+    Solver(int N, int M){
+        this.N = N;
+        this.M = M;
+
+        V_old = new Vec3field(N,M);
+        V_new = new Vec3field(N,M);
+
+        P_old = new floatfield(N,M);
+        P_new = new floatfield(N,M);
+
+        ro_old = new floatfield(N,M);
+        ro_new = new floatfield(N,M);
+    }
+
+    
+};
